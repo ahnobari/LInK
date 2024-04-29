cb9739d0760ffd2045a9c3921c99d31daeca1729
ahnobari <nobariam@gmail.com>
2024-04-29 19:32:29 +0000

    layer optimization added

--- LInK/CAD.py
+++ LInK/CAD.py
@@ -1,6 +1,6 @@
 import numpy as np
 from .Solver import solve_rev_vectorized_batch_CPU
-
+from scipy.optimize import milp, LinearConstraint
 
 def onSegment(p, q, r): 
     if ( (q[0] <= max(p[0], r[0])) and (q[0] >= min(p[0], r[0])) and 
@@ -102,3 +102,122 @@ def linkage_joint_collisions(A,x0,nt,tolerance=0.08, sol=None, start=0,end=2*np.
         collision_matrix[i,l2[i]] = False
     
     return collision_matrix
+
+def get_layers(A_, x0_, node_types_, sol_ = A_T, x0_T, node_types_T, sol_t, verbose=True):
+    O = linkage_collisions(A_, x0_, node_types_, sol=sol_)
+    O &= ~np.eye(O.shape[0],dtype=bool)
+    C = linkage_joint_collisions(A_, x0_, node_types_, sol=sol_).T
+    l1,l2 = np.where(np.triu(A_))
+    A = np.zeros([A_.shape[0],len(l1)])
+    A[l1,np.arange(len(l1))] = 1
+    A[l2,np.arange(len(l1))] = 1
+    A = A.astype(bool)
+    
+    desvar_zeros = np.zeros(O.shape[0]+2*C.shape[0]+C.sum()+O.sum())
+    z_idx = np.arange(0,O.shape[0])
+    u_idx = np.arange(O.shape[0],O.shape[0]+C.shape[0])
+    v_idx = np.arange(O.shape[0]+C.shape[0],O.shape[0]+2*C.shape[0])
+    y_idx = np.arange(O.shape[0]+2*C.shape[0],O.shape[0]+2*C.shape[0]+C.sum())
+    x_idx = np.arange(O.shape[0]+2*C.shape[0]+C.sum(),O.shape[0]+2*C.shape[0]+C.sum()+O.sum())
+
+    lt = 1.0
+    N = lt*O.shape[0]*100
+    
+    A1 = np.zeros((z_idx.shape[0],desvar_zeros.shape[0]))
+    A1[z_idx,z_idx] = 1
+    C1 = LinearConstraint(A1,lb=0, ub=(O.shape[0]+1)*lt*2)
+
+    A2 = []
+    lbs = []
+    counter = 0
+    for i in range(O.shape[0]):
+        col_i = np.where(O[i])[0]
+        for j in col_i:
+            row = np.zeros(desvar_zeros.shape[0])
+            row[z_idx[i]] = 1
+            row[z_idx[j]] = -1
+            row[x_idx[counter]] = N
+            A2.append(row)
+            lbs.append(lt)
+            
+            row = np.zeros(desvar_zeros.shape[0])
+            row[z_idx[i]] = -1
+            row[z_idx[j]] = 1
+            row[x_idx[counter]] = -N
+            A2.append(row)
+            lbs.append(lt-N)
+            
+            counter += 1
+
+    A2 = np.array(A2)
+    C2 = LinearConstraint(A2,lb=lbs)
+
+    A3 = np.zeros((1,desvar_zeros.shape[0]))
+    A3[0,z_idx[0]] = 1
+    C3 = LinearConstraint(A3,lb=0, ub=0)
+
+    A4 = []
+    ub = []
+    for j in range(C.shape[0]):
+        col_j = np.where(A[j])[0]
+        for i in col_j:
+            row = np.zeros(desvar_zeros.shape[0])
+            row[u_idx[j]] = 1
+            row[z_idx[i]] = -1
+            A4.append(row)
+            ub.append(0)
+            
+            row = np.zeros(desvar_zeros.shape[0])
+            row[v_idx[j]] = -1
+            row[z_idx[i]] = 1
+            A4.append(row)
+            ub.append(0)
+
+    A4 = np.array(A4)
+    C4 = LinearConstraint(A4,ub=ub)
+
+    A5 = []
+    ub = []
+    counter = 0
+    for j in range(C.shape[0]):
+        col_i = np.where(C[j])[0]
+        for i in col_i:
+            row = np.zeros(desvar_zeros.shape[0])
+            row[z_idx[i]] = -1
+            row[v_idx[j]] = 1
+            row[y_idx[counter]] = -N
+            A5.append(row)
+            ub.append(-lt)
+            
+            row = np.zeros(desvar_zeros.shape[0])
+            row[z_idx[i]] = 1
+            row[u_idx[j]] = -1
+            row[y_idx[counter]] = N
+            A5.append(row)
+            ub.append(N+lt)
+            
+            counter += 1 
+
+    A5 = np.array(A5)
+    C5 = LinearConstraint(A5,ub=ub)
+
+    A6 = np.zeros([y_idx.shape[0]+x_idx.shape[0],desvar_zeros.shape[0]])
+    A6[np.arange(y_idx.shape[0]),y_idx] = 1
+    A6[y_idx.shape[0]+np.arange(x_idx.shape[0]),x_idx] = 1
+    C6 = LinearConstraint(A6,lb=0, ub=1)
+
+    C_obj = np.zeros(desvar_zeros.shape[0])
+    C_obj[z_idx] = 1
+    C_obj[v_idx] = 1
+    C_obj[u_idx] = -1
+
+    integrality = np.zeros(desvar_zeros.shape[0])
+    integrality[y_idx] = 1
+    integrality[x_idx] = 1
+    
+    results = milp(C_obj,integrality=integrality,constraints=[C1,C2,C3,C4,C5,C6],options={'disp':verbose})
+    
+    if results.status != 0:
+        return desvar_zeros[z_idx], False
+    else:
+        return results.x[z_idx], True
